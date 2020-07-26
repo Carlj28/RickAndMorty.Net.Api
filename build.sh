e3a298ecc31cf5ed8272621429785e18ec50458c
bziolkowski <bartekziolkowski@hotmail.com>
2020-07-26 18:02:26 +0200

    And more debugging

--- build.sh
+++ build.sh
@@ -59,6 +59,11 @@ if [ ! -f "$CAKE_DLL" ]; then
     ls /home/travis/build/Carlj28/RickAndMorty.Net.Api/tools/
     echo "/home/travis/build/Carlj28/RickAndMorty.Net.Api/tools/Cake.Tool.0.38.4/"
     ls /home/travis/build/Carlj28/RickAndMorty.Net.Api/tools/Cake.Tool.0.38.4/
+    echo "/home/travis/build/Carlj28/RickAndMorty.Net.Api/tools/Cake.Tool.0.38.4/tools"
+    ls /home/travis/build/Carlj28/RickAndMorty.Net.Api/tools/Cake.Tool.0.38.4/tools
+    echo "/home/travis/build/Carlj28/RickAndMorty.Net.Api/tools/Cake.Tool.0.38.4/package"
+    ls /home/travis/build/Carlj28/RickAndMorty.Net.Api/tools/Cake.Tool.0.38.4/package
+    echo "end"
     if [ $? -ne 0 ]; then
         echo "An error occured while installing Cake."
         exit 1
