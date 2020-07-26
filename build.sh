23bf425a68329a5762a29ec11ec82bc35bf41202
bziolkowski <bartekziolkowski@hotmail.com>
2020-07-26 17:56:20 +0200

    Some debbuging of sh build script

--- build.sh
+++ build.sh
@@ -36,17 +36,17 @@ fi
 # INSTALL .NET CORE CLI
 ###########################################################################
 
-echo "Installing .NET CLI..."
-if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
-  mkdir "$SCRIPT_DIR/.dotnet"
-fi
-sudo apt-get install libunwind8
-curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh
-sudo bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version $DOTNET_VERSION  --install-dir .dotnet --no-path
-export PATH="$SCRIPT_DIR/.dotnet":$PATH
-export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-export DOTNET_CLI_TELEMETRY_OPTOUT=1
-"$SCRIPT_DIR/.dotnet/dotnet" --info
+#echo "Installing .NET CLI..."
+#if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
+#  mkdir "$SCRIPT_DIR/.dotnet"
+#fi
+#sudo apt-get install libunwind8
+#curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh
+#sudo bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version $DOTNET_VERSION  --install-dir .dotnet --no-path
+#export PATH="$SCRIPT_DIR/.dotnet":$PATH
+#export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+#export DOTNET_CLI_TELEMETRY_OPTOUT=1
+#"$SCRIPT_DIR/.dotnet/dotnet" --info
 
 
 ###########################################################################
@@ -55,6 +55,8 @@ export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
 if [ ! -f "$CAKE_DLL" ]; then
     curl -Lsfo Cake.Tool.zip "https://www.nuget.org/api/v2/package/Cake.Tool/$CAKE_VERSION" && unzip -q Cake.Tool.zip -d "$TOOLS_DIR/Cake.Tool.$CAKE_VERSION" && rm -f Cake.Tool.zip
+    ls /home/travis/build/Carlj28/RickAndMorty.Net.Api/tools/
+    ls /home/travis/build/Carlj28/RickAndMorty.Net.Api/tools/Cake.Tool.0.38.4/
     if [ $? -ne 0 ]; then
         echo "An error occured while installing Cake."
         exit 1
